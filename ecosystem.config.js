8340d8bb2660499933aa76ca805af5409ac9bf5e
Ravinder Singh <ravinders@valyoo.in>
2017-11-18 17:23:34 +0530

    Setup PM2 010

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-18-216-187-215.us-east-2.compute.amazonaws.com',
       key: '/Users/ravinder/Downloads/first-app.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:ravinder3128/tutorial-pt-2.git',
+      repo: 'ssh://git@github.com:ravinder3128/tutorial-pt-2.git',
       path: '/home/ec2-user/tutorial-pt-2',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
