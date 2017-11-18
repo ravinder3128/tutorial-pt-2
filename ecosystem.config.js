d1dc318d4a866aa0e19305ec8e8f20ce11d6c0ce
Ravinder Singh <ravinders@valyoo.in>
2017-11-18 18:48:57 +0530

    Changes 115

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:ravinder3128/tutorial-pt-2.git',
       path: '/home/ec2-user/tutorial-pt-2',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && pm2 Restart ecosystem.config.js'
     }
   }
 }
